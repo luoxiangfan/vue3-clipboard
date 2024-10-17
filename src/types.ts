d9d92e25bcd663ef6f3ca4fbad514a679f75541f
luoxiangfan <infiniment@163.com>
2024-10-17 11:07:01 +0800

    chore: use single file to implement type definition and export

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,13 @@
+import { App } from 'vue'
+
 export type Action = 'copy' | 'cut'
 
 export type ClipboardConfig = {
   autoSetContainer: boolean
   appendToBody: boolean
+}
+
+export interface IVueClipboard {
+  config(config: ClipboardConfig): void
+  install(app: App): void
 }
