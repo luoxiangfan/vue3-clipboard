59859d8147ed6184f614f66c5cba893cb2279f6b
lxf <infiniment@163.com>
2025-11-12 15:43:41 +0800

    chore(ESLint): TypeScript Configuration File

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,9 @@
-
-export default [
+import { defineConfig } from 'eslint/config'
+export default defineConfig([
   {
     ignores: ["dist/*", "node_modules/*", "coverage/*", 'coverage'],
     rules: {
       "unicorn/no-array-callback-reference": 0
     }
   }
-]
+])
